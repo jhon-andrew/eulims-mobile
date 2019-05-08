f81882d5d53ca180dc5503fb76be9824047ff8b0
Jhon Andrew Baes <anecbook@gmail.com>
2019-05-08 16:06:56 +0800

    * Refactored Login module. * Changed androidStatusbar and splash backgroundColor props to match the Login UI.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import Store from '../store'
-import { StyleSheet, Image, Dimensions, TouchableWithoutFeedback } from 'react-native'
-import { LinearGradient, SplashScreen } from 'expo'
-import { Container, Grid, Row, Text, View, H2, Form, Item, Input, Icon, Button, Content, Toast, Spinner } from 'native-base'
-import ServerSelection from './ServerSelection'
+import { SplashScreen, LinearGradient, Constants } from 'expo'
+import { StyleSheet, Dimensions, Keyboard, Image, TouchableWithoutFeedback } from 'react-native'
+import { Container, Content, View, H2, Text, Form, Item, Input, Icon, Button, Toast, Spinner } from 'native-base'
+import theme from '../../native-base-theme/variables/eulims'
 import { Login } from '../api'
 
 const styles = StyleSheet.create({
-  verticalCenter: {
-    alignItems: 'center',
-    padding: 8
+  verticallyCentered: {
+    flexDirection: 'column',
+    justifyContent: 'center'
   },
   logo: {
+    width: Dimensions.get('window').width * 0.8 - 20,
     resizeMode: 'contain',
-    alignSelf: 'center',
     borderRadius: 2,
-    width: Dimensions.get('window').width * 0.8 // 80% of App window
+    alignSelf: 'center'
   },
   branding: {
-    marginBottom: 8,
     textAlign: 'center',
     color: '#ffffff',
     textShadowColor: 'rgba(0, 0, 0, 0.2)',
     textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 2
+    textShadowRadius: 2,
+    marginBottom: 8
   },
   formItem: {
     marginVertical: 4,
@@ -43,7 +43,7 @@ class LoginScreen extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      isOpen: false,
+      dynamicHeight: Dimensions.get('window').height - Constants.statusBarHeight - theme.toolbarHeight - 20,
       prefServer: undefined,
       email: undefined,
       password: undefined,
@@ -52,18 +52,45 @@ class LoginScreen extends React.Component {
   }
 
   componentDidMount () {
-    let { getParam } = this.props.navigation
     SplashScreen.hide()
-    if (getParam('message')) Toast.show({
-      text: getParam('message'),
-      buttonText: 'Okay',
-      duration: 3000
+
+    this.listeners = [
+      Keyboard.addListener('keyboardDidShow', ({ endCoordinates }) => {
+        this.changeViewHeight(endCoordinates.height)
+      }),
+      Keyboard.addListener('keyboardDidHide', () => {
+        this.changeViewHeight()
+      }),
+      this.props.navigation.addListener('didFocus', () => {
+        this.setState({ prefServer: this.props.store.get('prefServer') })
+      })
+    ]
+
+    const { navigation, store } = this.props
+
+    if (navigation.getParam('message')) {
+      Toast.show({
+        text: navigation.getParam('message'),
+        buttonText: 'Okay',
+        duration: 3000
+      })
+    }
+  }
+
+  componentWillUnmount () {
+    this.listeners.forEach((listener, index) => {
+      listener.remove()
     })
-    this.props.store.on('prefServer').subscribe(prefServer => this.setState({ prefServer }))
+  }
+
+  changeViewHeight (offset = 0) {
+    this.setState({
+      dynamicHeight: Dimensions.get('window').height - Constants.statusBarHeight - theme.toolbarHeight - 20 - offset
+    })
   }
 
   async login () {
-    let { email, password, prefServer } = this.state
+    const { email, password, prefServer } = this.state
 
     this.setState({
       emailError: !this.state.email,
@@ -74,17 +101,12 @@ class LoginScreen extends React.Component {
     if (!email || !password || !prefServer) return false
 
     this.setState({ loggingIn: true })
-    let login = await Login(this.state.email, this.state.password)
+    const login = await Login(this.state.email, this.state.password)
     const { store, navigation } = this.props
 
     if (login && login.token) {
       store.set('token')(login.token)
       store.set('user')(login.user)
-      Toast.show({
-        text: `Welcome ${login.user.firstName}!`,
-        buttonText: 'Okay',
-        duration: 3000
-      })
       return navigation.navigate('sampleTagging')
     } else if (login && login.error) {
       Toast.show({
@@ -98,49 +120,42 @@ class LoginScreen extends React.Component {
   }
 
   render () {
-    const { store, navigation } = this.props
+    const { navigation, store } = this.props
 
     return (
       <Container>
         <LinearGradient colors={['#b1d1e4', '#3c8dbc']} start={[0.0, 0.5]} end={[0.5, 1.0]} style={{ flex: 1 }}>
-          <Content style={{ flexGrow: 1 }}>
-            <Grid style={{ height: Dimensions.get('window').height - 20 }}>
-              <Row style={styles.verticalCenter}>
-                <View style={{ flex: 1, flexDirection: 'column' }}>
-                  <Image source={require('../../assets/onelab-512.png')} style={styles.logo} />
+          <Content padder>
+            <View style={[styles.verticallyCentered, { minHeight: this.state.dynamicHeight }]}>
+              <Image source={require('../../assets/onelab-512.png')} style={styles.logo} />
+              <H2 style={styles.branding}>Enhanced Unified Laboratory Information Management System</H2>
+              <Text style={styles.branding}>Department of Science and Technology</Text>
 
-                  <H2 style={styles.branding}>Enhanced Unified Laboratory Information Management System</H2>
-                  <Text style={styles.branding}>Department of Science and Technology</Text>
-                  
-                  <Form style={{ marginVertical: 4 }}>
-                    <Item rounded style={styles.formItem} disabled={this.state.loggingIn} error={this.state.emailError}>
-                      <Input placeholder="Email" autoCapitalize="none" autoComplete="email" keyboardType="email-address" onChangeText={email => this.setState({ email })} disabled={this.state.loggingIn} />
-                      <Icon type="MaterialCommunityIcons" name="email" />
+              <Form style={{ marginVertical: 4 }}>
+                <Item rounded style={styles.formItem} disabled={this.state.loggingIn} error={this.state.emailError}>
+                  <Input placeholder="Email" autoCapitalize="none" autoComplete="email" keyboardType="email-address" onChangeText={email => this.setState({ email })} disabled={this.state.loggingIn} />
+                  <Icon type="MaterialCommunityIcons" name="email" />
+                </Item>
+                <Item rounded style={styles.formItem} disabled={this.state.loggingIn} error={this.state.passwordError}>
+                  <Input placeholder="Password" secureTextEntry={true} onChangeText={password => this.setState({ password })} disabled={this.state.loggingIn} />
+                  <Icon type="MaterialCommunityIcons" name="textbox-password" />
+                </Item>
+                <TouchableWithoutFeedback onPress={() => navigation.navigate('serverSelection')}>
+                  <View pointerEvents="box-only" style={{padding: 0}}>
+                    <Item rounded style={styles.formItem} error={this.state.prefServerError}>
+                      <Input placeholder="Server" autoCapitalize="none" editable={false} value={store.get('prefServer')} onChangeText={prefServer => this.setState({ prefServer })} />
+                      <Icon type="MaterialCommunityIcons" name="server" />
                     </Item>
-                    <Item rounded style={styles.formItem} disabled={this.state.loggingIn} error={this.state.passwordError}>
-                      <Input placeholder="Password" secureTextEntry={true} onChangeText={password => this.setState({ password })} disabled={this.state.loggingIn} />
-                      <Icon type="MaterialCommunityIcons" name="textbox-password" />
-                    </Item>
+                  </View>
+                </TouchableWithoutFeedback>
+              </Form>
 
-                    <TouchableWithoutFeedback onPress={() => navigation.navigate('serverSelection')}>
-                      <View pointerEvents="box-only" style={{padding: 0}}>
-                        <Item rounded style={styles.formItem} error={this.state.prefServerError}>
-                          <Input placeholder="Server" autoCapitalize="none" editable={false} value={store.get('prefServer')} onChangeText={prefServer => this.setState({ prefServer })} />
-                          <Icon type="MaterialCommunityIcons" name="server" />
-                        </Item>
-                      </View>
-                    </TouchableWithoutFeedback>
+              <Button block rounded style={{ marginVertical: 4 }} onPress={this.login.bind(this)} disabled={this.state.loggingIn}>
+                { this.state.loggingIn ? (<Spinner color="#ffffff" />) : (<Text>Login</Text>) }
+              </Button>
 
-                  </Form>
-
-                  <Button block rounded style={{ marginVertical: 4 }} onPress={() => this.login()} disabled={this.state.loggingIn}>
-                    { this.state.loggingIn ? (<Spinner color="#ffffff" />) : (<Text>Login</Text>) }
-                  </Button>
-
-                  <Text style={styles.footer}>EULIMS Mobile v0.1 Beta</Text>
-                </View>
-              </Row>
-            </Grid>
+              <Text style={styles.footer}>EULIMS Mobile v0.1 Beta</Text>
+            </View>
           </Content>
         </LinearGradient>
       </Container>
